56747e7e522ef0abc9626a84ce5fdefbaa6b21b8
Weihan Zu <w.zu@birdcontrolgroup.com>
2017-08-30 16:05:44 +0200

    mango hello test

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("Hello! This is my first C program with Ubuntu 16.04 LTS\n");
+    /* Do something more if you want */
+
+}
+
+
+/*
+#include <iostream>
+
+int main(int argc, char** argv[]){
+
+}*/
